0fa7ca90b0fc5e90e1678ef73706cd733809c63e
Nilay Nath Sharan <nilaynathsharan16@gmail.com>
2022-07-02 16:51:59 +0530

    config files

--- test/brownie-test/scripts/deploy.py
+++ test/brownie-test/scripts/deploy.py
@@ -1,8 +1,8 @@
-from brownie import accounts
+from brownie import accounts, config
 
 
 def deploy_simple_storage():
-    account = accounts.load("main_account")
+    account = accounts.add(config["wallets"]["from_key"])
     print(account)
 
 
